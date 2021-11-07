077e9b8f4de8799565024dbfe8bf731edad2edf1
Jakob123321 <87225819+Jakob123321@users.noreply.github.com>
2021-11-07 19:18:51 +0100

    Update BookingRepository.java

--- src/main/java/de/thb/carsharing/repository/BookingRepository.java
+++ src/main/java/de/thb/carsharing/repository/BookingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.RepositoryDefinition;
 
 @RepositoryDefinition(domainClass = Booking.class, idClass = Long.class)
 public interface BookingRepository extends CrudRepository<Booking, Long> {
-    //Queries
+    // Queries
 }
