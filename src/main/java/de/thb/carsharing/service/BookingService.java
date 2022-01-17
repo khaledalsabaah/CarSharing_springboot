efd59f61e400e168ac8f24365d025c5c879ead3d
BuildTools <unconfigured@null.spigotmc.org>
2022-01-17 18:32:47 +0100

    added automatic cancellation of booking after 15 minutes of reservation

--- src/main/java/de/thb/carsharing/service/BookingService.java
+++ src/main/java/de/thb/carsharing/service/BookingService.java
@@ -1,17 +1,19 @@
 package de.thb.carsharing.service;
 
 import de.thb.carsharing.entity.Booking;
-import de.thb.carsharing.entity.Car;
 import de.thb.carsharing.entity.Type.BookingStatus;
 import de.thb.carsharing.repository.BookingRepository;
 import de.thb.carsharing.repository.CarRepository;
 import de.thb.carsharing.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -22,6 +24,7 @@ public class BookingService {
     private final BookingRepository bookingRepository;
     private final CarRepository carRepository;
     private final CustomerRepository customerRepository;
+    private final ThreadPoolTaskScheduler threadPoolTaskScheduler;
 
     public List<Booking> getAllBookings() {
         return (List<Booking>) bookingRepository.findAll();
@@ -43,13 +46,17 @@ public class BookingService {
                             booking.getBookingStatus().equals(BookingStatus.STARTED)).collect(Collectors.toList());
             if (conflictingBookings.isEmpty()) {
                 carRepository.findById(carID).get().setAvailable(false);
-                return bookingRepository.save(Booking.builder()
+                Booking booking = bookingRepository.save(Booking.builder()
                         .car(carRepository.findById(carID).get())
                         .customer(customerRepository.findById(customerID).get())
                         .bookTime(new Date())
                         .bookingStatus(BookingStatus.BOOKED)
                         .cost(0)
                         .build());
+                threadPoolTaskScheduler.schedule(
+                        new CancelBookingAfterFifteenMinutes(booking.getId()),
+                        new Date(booking.getBookTime().getTime() + TimeUnit.MINUTES.toMillis(15)));
+                return booking;
             } else {
                 return null; //TODO Exceptions
             }
@@ -86,6 +93,26 @@ public class BookingService {
             return true;
         } else
             return false;
+    }
+
+    class CancelBookingAfterFifteenMinutes implements  Runnable {
+
+        private long id;
+
+        public CancelBookingAfterFifteenMinutes(long id){
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            if (bookingRepository.existsById(id)){
+                Booking booking = bookingRepository.findById(id).get();
+                if (booking.getBookingStatus().equals(BookingStatus.BOOKED)) {
+                    booking.setBookingStatus(BookingStatus.CANCELLED);
+                    bookingRepository.save(booking);
+                }
+            }
+        }
     }
 
     public boolean finishBooking(long id) {
